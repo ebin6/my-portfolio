810f104bed65a0ca2427c7f304756c690074d5ae
ebin6 <219611582+ebin6@users.noreply.github.com>
2026-04-06 10:26:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/Index.tsx
@@ -0,0 +1,27 @@
+import { HeroSection } from "@/components/portfolio/HeroSection";
+import { AboutSection } from "@/components/portfolio/AboutSection";
+import { SkillsSection } from "@/components/portfolio/SkillsSection";
+import { ExperienceSection } from "@/components/portfolio/ExperienceSection";
+import { ProjectsSection } from "@/components/portfolio/ProjectsSection";
+import { TestimonialsSection } from "@/components/portfolio/TestimonialsSection";
+import { ContactSection } from "@/components/portfolio/ContactSection";
+import { Navbar } from "@/components/portfolio/Navbar";
+import { Footer } from "@/components/portfolio/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <SkillsSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <TestimonialsSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
